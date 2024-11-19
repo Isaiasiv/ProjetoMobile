07027c0ec714440af3cd41aa4ab1ecc61fc2257a
Isaiasiv <isaias.ribeiro@estudante.ifgoiano.edu.br>
2024-11-19 10:48:36 -0300

    criado as estruturas das pastas e as telas de login, e de alerta, feito a adiçao da logo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.projetomobile"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.projetomobile"
