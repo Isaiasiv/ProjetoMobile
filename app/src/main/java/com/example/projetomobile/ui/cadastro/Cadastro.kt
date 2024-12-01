f133abe4933dae57c23a9e04577356e73c6600ac
Isaiasiv <isaias.ribeiro@estudante.ifgoiano.edu.br>
2024-12-01 13:31:41 -0300

    telas de login, perfil, e cadastro prontas e a configuração de conexão do projeto com o firebase feita, criação de icons de email user e senha, e palavras usadas nas string seguindo boas praticas

--- app/src/main/java/com/example/projetomobile/ui/cadastro/Cadastro.kt
+++ app/src/main/java/com/example/projetomobile/ui/cadastro/Cadastro.kt
@@ -1,16 +1,17 @@
-package com.example.projetomobile
+package com.example.projetomobile.ui.cadastro
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.projetomobile.R
 
-class MainActivity : AppCompatActivity() {
+class Cadastro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_cadastro)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/projetomobile/ui/login/Login.kt
+++ app/src/main/java/com/example/projetomobile/ui/login/Login.kt
@@ -1,22 +1,49 @@
 package com.example.projetomobile.ui.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.setPadding
 import com.example.projetomobile.R
+import com.example.projetomobile.ui.cadastro.Cadastro
 
 class Login : AppCompatActivity() {
+
+    private lateinit var textTelaCadastro: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+
+
+        // Configurar margens para as barras do sistema
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Inicializar os componentes
+        iniciarComponents()
+
+        // Configurar o clique no TextView
+        configurarCliqueCadastro()
+    }
+
+    // Inicializar os componentes
+    private fun iniciarComponents() {
+        textTelaCadastro = findViewById(R.id.textCadastro)
+    }
+
+    // Configurar ação para o clique do TextView
+    private fun configurarCliqueCadastro() {
+        textTelaCadastro.setOnClickListener {
+            val intent = Intent(this@Login, Cadastro::class.java)
+            startActivity(intent)
         }
     }
 }
